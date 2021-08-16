094e59e5463eac731bde9ea1f83567959631c094
Kim Young Geun <smart5265988@gmail.com>
2021-08-16 17:06:33 +0900

    day2 webSocket

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
+import http from 'http';
 import express from 'express';
+import WebSocket from 'ws';
 
 const app = express();
 
@@ -9,4 +11,32 @@ app.get('/', (req, res) => res.render('home'));
 app.get('/*', (req, res) => res.redirect('/'));
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
-app.listen(3000, handleListen);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server }); // http wss 둘다 가능하게함 (같은 서버에서)
+
+const sockets = [];
+
+wss.on('connection', (socket) => {
+  sockets.push(socket);
+  socket['nickname'] = 'Anon';
+  console.log('Connected to Browser ✅');
+
+  socket.on('close', () => {
+    console.log('Disconnected from Browser ❌');
+  });
+
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg);
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach((aSocket) => {
+          aSocket.send(`${socket.nickname}: ${message.payload}`);
+        });
+      case 'nickname':
+        socket['nickname'] = message.payload;
+    }
+  });
+});
+
+server.listen(3000, handleListen);
